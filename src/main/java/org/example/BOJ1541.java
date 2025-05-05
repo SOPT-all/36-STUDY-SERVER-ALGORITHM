a27f1932784e51041f015b0f854d5e570c683b83
LSM <leesung2925@gmail.com>
2025-05-05 17:28:12 +0900

    [feat] week 1 complete

--- src/main/java/org/example/BOJ1541.java
+++ src/main/java/org/example/BOJ1541.java
@@ -1,4 +1,41 @@
 package org.example;
 
+import java.io.*;
+
 public class BOJ1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String line = br.readLine();
+        StringBuilder num = new StringBuilder();
+        int sum = 0;
+
+        boolean minusFlag = false;
+
+        for(int i = 0; i < line.length(); i++){
+            char cur = line.charAt(i);
+            if(cur != '-' && cur != '+'){
+                num.append(cur);
+                if(i != line.length() - 1) continue;
+            }
+
+            if(minusFlag){
+                sum -= Integer.parseInt(num.toString());
+            }else{
+                sum += Integer.parseInt(num.toString());
+            }
+            num = new StringBuilder();
+
+
+            if(cur == '-'){
+                minusFlag = true;
+            }
+        }
+        bw.append(String.valueOf(sum));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
--- src/main/java/org/example/BOJ1715.java
+++ src/main/java/org/example/BOJ1715.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.io.*;
+import java.util.PriorityQueue;
+
 public class BOJ1715 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for(int i = 0; i < n; i++){
+            queue.add(Integer.parseInt(br.readLine()));
+        }
+
+        int sum = 0;
+
+        while(!queue.isEmpty()){
+            if(queue.size() == 1){
+                break;
+            }
+
+            int x = queue.poll();
+            int y = queue.poll();
+
+            queue.add(x + y);
+            sum += x + y;
+        }
+        bw.append(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
